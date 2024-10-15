1210da8cb1b65449c0d754a7147dd4118d4faada
sajib689 <jibonraja11@gmail.com>
2024-10-15 16:05:10 +0600

    inheritence add

---
+++ src/inheritence.ts
@@ -0,0 +1,18 @@
+class Parent {
+    name: string;
+    age: number;
+    address: string;
+    constructor(name: string, age: number, address: string) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+}
+
+class Student extends Parent{
+    constructor(name: string, age: number, address: string){
+        super(name, age, address);
+    }
+}
+const studentInfo = new Student('sajib', 24, 'Jashore')
