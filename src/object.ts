42bf5577f0e7fe3493ae4fd1644af34fd4073d6e
sajib689 <jibonraja11@gmail.com>
2024-10-15 11:32:47 +0600

    object orinated typescript

--- src/object.ts
+++ src/object.ts
@@ -13,3 +13,23 @@ class Animal {
 }
 
 const dog = new Animal('German Shapred', 'dog', 'Ghew Ghew');
+dog.makeSound();
+
+class PersonalInfo {
+    name: string;
+    age: number;
+    education: string;
+    results: number;
+    constructor(name: string, age: number, education: string, results: number) {
+        this.name = name;
+        this.age = age;
+        this.education = education;
+        this.results = results;
+    }
+    makePersonalInfo () {
+        console.log(`He is ${this.name} and age ${this.age} have ${this.education} and the result is ${this.results}`)
+    }
+}
+
+const man = new PersonalInfo('sajib', 24, 'BA', 4.28)
+man.makePersonalInfo()
