83695897ae3d28c3e5fe39cb9c9be6fd3a9e8972
sajib689 <jibonraja11@gmail.com>
2024-10-15 16:22:39 +0600

    inheritence

--- src/inheritence.ts
+++ src/inheritence.ts
@@ -14,5 +14,23 @@ class Student extends Parent{
     constructor(name: string, age: number, address: string){
         super(name, age, address);
     }
+    makeStudent() {
+        console.log(`I am ${this.name} and I am ${this.age} years old. I live in ${this.address}`)
+    }
 }
 const studentInfo = new Student('sajib', 24, 'Jashore')
+studentInfo.makeStudent()
+
+class Student1 extends Parent {
+    desgination: string;
+    constructor(name: string, age: number, address: string, desgination: string) {
+        super(name, age, address);
+        this.desgination = desgination; 
+    }
+    makeStudent2(): string {
+       return `I am ${this.name} Hossen and age ${this.age} years old and I live in ${this.address} and I am a ${this.desgination}`;
+    }
+}
+
+const studentInfo1 = new Student1('sajib', 24, 'Jashore', 'Frontend Developer');
+console.log(studentInfo1.makeStudent2());
